99d3234aa5ea601c8c32e4794c767618c31bf5a0
ggobbe <guillaume.gobbe@gmail.com>
2015-03-16 12:14:58 +0000

    Time elapsed is displayed when the program ends

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
-	_ "github.com/goinaction/code/chapter2/sample/matchers"
-	"github.com/goinaction/code/chapter2/sample/search"
+	_ "github.com/ggobbe/goinaction/chapter2/sample/matchers"
+	"github.com/ggobbe/goinaction/chapter2/sample/search"
 )
+
+var start time.Time
 
 // init is called prior to main.
 func init() {
 	// Change the device for logging to stdout.
 	log.SetOutput(os.Stdout)
+	start = time.Now()
 }
 
 // main is the entry point for the program.
 func main() {
 	// Perform the search for the specified term.
 	search.Run("president")
+
+	fmt.Printf("Time elapsed: %s\n", time.Now().Sub(start))
 }
